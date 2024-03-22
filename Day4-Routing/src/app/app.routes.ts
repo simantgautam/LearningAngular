3d1cf7a60e7550a13c14e9e3dfdd58eb9a6457eb
simantgautam <gautam8178838469@gmail.com>
2024-03-23 02:58:34 +0530

    Routing

---
+++ Day4-Routing/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+export const routes: Routes = [
+  { path: '', title: 'Home', component: HomeComponent },
+  { path: 'about', title: 'About', component: AboutComponent },
+  { path: 'product', title: 'Product', component: ProductComponent },
+  {
+    path: 'product',
+    children: [
+      {
+        path: 'productDetail',
+        title: 'Product | Product Detail',
+        component: ProductDetailsComponent,
+      },
+    ],
+  },
+  { path: '**', title: 'Page Not Found', component: PageNotFoundComponent },
+];
